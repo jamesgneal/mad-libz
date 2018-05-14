5a9a1048f68e8a2666ef027a2e81b7fa341d6d6d
Unknown <cgzoghby@gmail.com>
2018-05-14 16:05:58 -0400

    Fixed story posting issues

--- public/assets/js/client.js
+++ public/assets/js/client.js
@@ -1,31 +1,19 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
-//So, need for a listener to be attached that looks for true devour statuses (a la sleepy with the cats)
-//and if devour is true, then display them in a separate column.
 $(function () {
+  //fairly straightforward, just grab words from form and make an ajax call.
   $("#storyAdd").on("submit", function (event) {
     event.preventDefault();
     var newStory = {
       story: $("#storyForm").val().trim()
-    }
+    };
     $.ajax("/api/madlibz/stories", {
       type: "POST",
-      words: newStory
-    }).then(function () {
-      console.log("Uploaded Story!")
-      location.reload();
-    })
-  })
-
-  $("#storyLoad").on("submit", function (event) {
-    event.preventDefault();
-    $.ajax("/api/madlibz/stories", {
-      type: "GET"
+      data: newStory
     }).then(
       function () {
-        console.log("Fetched a story!")
         location.reload();
-      }
-    )
+        console.log("Uploaded Story!")
+      })
   })
 
   $("#wordAdd").on("submit", function (event) {
@@ -40,9 +28,9 @@ $(function () {
       data: newWord
     }).then(
       function () {
-        console.log("created new madlib");
         // Reload the page to get the updated list
         location.reload();
+        console.log("created new madlib");
       }
     );
   });

3089a998a8b6e6363e51b67a3630131c66736413
Unknown <cgzoghby@gmail.com>
2018-05-14 21:40:42 -0400

    Working Text Replacer
    You can now post a story, load it, and write text and replace the text and get your story

--- controllers/madlib_controller.js
+++ controllers/madlib_controller.js
@@ -8,21 +8,44 @@ var db = require("../models"),
   madLib = db.MadLib;
 story = db.Story;
 
+//need sequelize for logic
+var Sequelize = require("../models").Sequelize
+
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function (req, res) {
   //the first route pulls all entered words.
-  madLib.findAll({}).then(function (result) {
-    var handleBarsObj = {
-      madLibs: result
-    };
-    res.render("index", handleBarsObj);
-  })
-  //second route pulls all entered stories.
-  story.findAll({}).then(function (result) {
-    console.log(result) //this is how we prove it pulls the data we entered. Accessed through Story.dataValues.words
-    //we will render through a handelbars "partial", I believe? We will ideally load the story at the same time as 
-    //mad lib words, and then render the story onto a partial template within the index.handlebars file.
-  })
+  madLib.findAll({}).then(function (result1) {
+    story.find({order: [Sequelize.fn('RAND')]}).then(function (result2) {
+      //capture the words of the story that was randomly found
+      var storyPlaceholder = result2.dataValues.words;
+      //search for all words that need to be replaced
+      var requiredInputsArray = storyPlaceholder.match(/\b[A-Z0-9]+\b/g);
+      //this prunes for "I" and other capitalized single-character words.
+      for (var i = 0; i < requiredInputsArray.length; i++) {
+        if (requiredInputsArray[i].length===1) {
+          requiredInputsArray.splice(i, 1)
+        }
+      };
+      //sanitycheck the for loop
+      console.log(requiredInputsArray);
+      var handleBarsObj = {
+        madLibs: result1,
+        story: storyPlaceholder,
+        POS: requiredInputsArray
+      };
+      res.render("index", handleBarsObj);
+      console.log(result1); //test to know that they are logged correctly
+      console.log(result2); //test to know that they are logged correctly
+      console.log(storyPlaceholder);
+      console.log(storyPlaceholder.match(/\b[A-Z0-9]+\b/g));
+      
+
+      //now, we take the pulled up story, and listen for caps words (VERB NOUN ADVERB etc)
+      //take those words and generate entry fields for each one, with suggestions based on the type of word.
+      //user enters words. Then their entries are captured in the DOM, and replace the caps words, then the story is displayed
+      //afterwards, that story is saved under their userID.
+    });
+  });
 });
 
 router.post("/api/madlibz", function (req, res) {
